d64fafe5c4715a019aa059aa09e9ec27556b7412
Randy Burgess <wrburgess@wrburgess.com>
2012-03-18 19:37:22 -0500

    initial commit and file copy

---
+++ README.md
@@ -0,0 +1,12 @@
+# gamepiece
+
+## about
+* desc: an api for managing items used for point accumulation in gaming
+* stack: 
+    * linux
+    * ruby
+    * sinatra
+* doc standard: [TomDoc](http://tomdoc.org/)
+
+## todo
+* change readme.txt to ALLOW when ready
---
+++ app.rb
@@ -0,0 +1,18 @@
+#lib
+require 'sinatra'
+
+#config
+class App
+  def self.name
+    "Sinatra Heroku Boostrap"
+  end
+end
+
+#GET root
+get '/' do
+  @title = App.name + ' | Home'
+  @body_id = 'home'
+  @body_class = 'page'
+
+  erb :index, :layout => :'layout'
+end
